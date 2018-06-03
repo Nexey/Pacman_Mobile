161bf49c3a0d22717946b882f0b9b78eb70222d6
SmileEdge <couderc.arnaud@yahoo.fr>
2018-06-03 19:28:56 +0200

    Menu avec bouttons bien avancé

--- core/src/com/gdx/game/view/Screens/MenuScreen.java
+++ core/src/com/gdx/game/view/Screens/MenuScreen.java
@@ -20,25 +20,31 @@ public class MenuScreen implements Screen {
     private Stage stage;
     Animation<TextureRegion> animation;
     float elapsed;
-    private Texture Texturebutton;
-    private TextureRegion TextureRegionButton;
-    private TextureRegionDrawable TexRegionDrawableButton;
-    private ImageButton button;
+    private Texture Texturebutton_play, Texturebutton_quit;
+    private TextureRegion TextureRegionButton_play, TextureRegionButton_quit;
+    private TextureRegionDrawable TexRegionDrawableButton_play, TexRegionDrawableButton_quit;
+    private ImageButton button_play, button_quit;
 
     public MenuScreen(final PacManGdx game){
         this.game = game;
         stage = new Stage(game.screenPort);
         Gdx.input.setInputProcessor(stage);
-        Texturebutton = new Texture(Gdx.files.internal("button.png"));
-        TextureRegionButton = new TextureRegion(Texturebutton);
-        TexRegionDrawableButton = new TextureRegionDrawable(TextureRegionButton);
-        button = new ImageButton(TexRegionDrawableButton);
+        Texturebutton_play = new Texture(Gdx.files.internal("play_button.png"));
+        Texturebutton_quit = new Texture(Gdx.files.internal("quit_button.png"));
+        TextureRegionButton_play = new TextureRegion(Texturebutton_play);
+        TextureRegionButton_quit = new TextureRegion(Texturebutton_quit);
+        TexRegionDrawableButton_play = new TextureRegionDrawable(TextureRegionButton_play);
+        TexRegionDrawableButton_quit = new TextureRegionDrawable(TextureRegionButton_quit);
+        button_play = new ImageButton(TexRegionDrawableButton_play);
+        button_quit = new ImageButton(TexRegionDrawableButton_quit);
         float screenWidth = Gdx.graphics.getWidth(), screenHeight = Gdx.graphics.getHeight();
-        button.setPosition( screenWidth/8,  screenHeight- 2*screenHeight/10);
-        stage.addActor(button);
+        button_play.setPosition( screenWidth/3,  screenHeight- screenHeight/5);
+        button_quit.setPosition( screenWidth/3,  screenHeight/2);
+        stage.addActor(button_play);
+        stage.addActor(button_quit);
         animation = GifDecoder.loadGIFAnimation(Animation.PlayMode.LOOP, Gdx.files.internal("pacman.gif").read());
 
-        button.addListener(new InputListener(){
+        button_play.addListener(new InputListener(){
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button)
             {
@@ -47,15 +53,14 @@ public class MenuScreen implements Screen {
             }
         });
 
-        /*stage.addListener(new InputListener(){
+        button_quit.addListener(new InputListener(){
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button)
             {
-                game.gotoGameScreen();
-                return true;
+               Gdx.app.exit();
+               return false;
             }
-            });*/
-
+        });
     }
 
 
